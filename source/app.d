8675a312ee314c3cc5d8275ea179d238b282f74c
Evgeniy <lysenko765@yandex.ru>
2023-03-23 01:23:04 +0300

    Serializable transaction model

--- source/app.d
+++ source/app.d
@@ -1,13 +1,27 @@
 import std.stdio : writeln;
-import uranium.db : Uranium, Op;
+import uranium.db;
+import std.concurrency;
 
 void main()
 {
-    auto db = new Uranium!string("smiles_db.um");
+    auto db = spawn({ new Uranium!string("smiles_db.um").runRequestReceiver(); });
     // CRUD operations
-    db("C O CC COC CC(=O)O"); //add elements: O CC COC CC(=O)O
+    db.run("C O CC COC CC(=O)O"); //add elements: O CC COC CC(=O)O
     // db(Op.C, " O ", "CC COC CC(=O)O"); - the same
-    writeln(db("R 5")); // get 5 random elements
-    db("U CC C1=CC=CC=C1"); // replace CC on C1=CC=CC=C1
-    db(Op.D, "C1=CC=CC=C1", "C"); // delete C1=CC=CC=C1 and C
+    writeln(db.run("R 5")); // get 5 random elements
+    // db("U CC C1=CC=CC=C1"); // replace CC on C1=CC=CC=C1
+    // db(Op.D, "C1=CC=CC=C1", "C"); // delete C1=CC=CC=C1 and C
+    // 
+    auto tr1 = db.newTransaction;
+    tr1("C CN")("C CCl")("R 5");
+    tr1("U CCl CCCl");
+    writeln(tr1.commit);
+
+    db.send(thisTid, "close");
+}
+
+Response run(ref Tid dbId, string request)
+{
+    dbId.send(thisTid, request);
+    return receiveOnly!(immutable(Response)[])[0];
 }
--- source/uranium/db.d
+++ source/uranium/db.d
@@ -1,5 +1,16 @@
 module uranium.db;
+
 import std.stdio : writeln;
+
+import std.conv : to;
+import std.array : appender, split;
+import std.concurrency : send, receiveOnly, spawn, Tid, thisTid;
+import std.random : randomCover, MinstdRand0, randomSample, unpredictableSeed;
+import std.range : array;
+import std.algorithm : filter;
+import std.uni : isWhite;
+import std.algorithm.iteration : joiner;
+import std.string : strip;
 
 enum ResponseType : byte
 {
@@ -53,7 +64,6 @@ enum Op : byte
   */
 class Uranium(V)
 {
-    import std.conv : to;
 
 private:
     V[] _state;
@@ -65,10 +75,6 @@ private:
 
     V[] get(size_t n)
     {
-        import std.random : randomCover, MinstdRand0, randomSample, unpredictableSeed;
-        import std.range : array;
-        import std.algorithm : filter;
-
         auto rnd = MinstdRand0(unpredictableSeed);
 
         version (D_LP64) // https://issues.dlang.org/show_bug.cgi?id=15147
@@ -93,70 +99,46 @@ private:
             update(v, "");
     }
 
-public:
-
-    this(string filename)
-    {
-        import std.array : appender;
-        import std.stdio : File;
-        import std.algorithm : filter;
-
-        auto state = appender!(V[]);
-        auto reader = File(filename, "r");
-        auto lines = reader.byLine().filter!"!a.empty"();
-        foreach (ref line; lines)
-        {
-            state ~= line.to!string;
-        }
-
-        // debug writeln("after read ", state);
-        _state = cast(V[]) state[];
-    }
-
     /**
-	 * Call Uranium using any type T that can be converted to a string
-	 *
-	 * Examples:
-	 * auto db = new Uranium!string("smiles_db.um");
-	 * ---
-	 * db(Op.C, " O ", "CC COC CC(=O)O"); add elements: O CC COC CC(=O)O
+     * Call Uranium using any type T that can be converted to a string
+     *
+     * Examples:
+     * auto db = new Uranium!string("smiles_db.um");
+     * ---
+     * db(Op.C, " O ", "CC COC CC(=O)O"); add elements: O CC COC CC(=O)O
      * writeln(db(Op.R, "5")); // get 5 random elements
      * db(Op.U "CC", "C1=CC=CC=C1"); // replace CC on C1=CC=CC=C1
      * db(Op.D, "C1=CC=CC=C1", "C"); // delete C1=CC=CC=C1 and "C"
-	 * ---
-	 */
-    R opCall(R = Response, T...)(Op op, T args)
-    {
-        import std.array : appender;
+     * ---
+     */
+    // R opCall(R = Response, T...)(Op op, T args)
+    // {
+    //     auto request = appender!string;
+    //     request.reserve(70);
+    //     request ~= op.to!string;
 
-        auto request = appender!string;
-        request ~= op.to!string;
+    //     foreach (arg; args)
+    //         request ~= arg.to!string;
 
-        foreach (arg; args)
-            request ~= arg.to!string;
-
-        return opCall(cast(string) request[]);
-    }
+    //     return opCall(cast(string) request[]);
+    // }
 
     /**
-	 * Call Uranium using request string
-	 *
-	 * Examples:
-	 * auto db = new Uranium!string("smiles_db.um");
-	 * ---
-	 * db("C O CC COC CC(=O)O"); // add elements: O CC COC CC(=O)O
+     * Call Uranium using request string
+     *
+     * Examples:
+     * auto db = new Uranium!string("smiles_db.um");
+     * ---
+     * db("C O CC COC CC(=O)O"); // add elements: O CC COC CC(=O)O
      * db("R 5");                // get 5 random elements
      * db("U CC C1=CC=CC=C1");   // replace CC on C1=CC=CC=C1
      * db("D C1=CC=CC=C1");      // delete C1=CC=CC=C1
-	 * ---
-	 */
-    R opCall(R = Response)(string request)
-    {
-        import std.array : split;
-        import std.uni : isWhite;
-        import std.algorithm.iteration : joiner;
-        import std.string : strip;
+     * ---
+     */
+    /*    R opCall(R = Response)(string request) { } */
 
+    R execute(R = Response)(string request)
+    {
         string[] p = request.strip.split!isWhite;
         switch (p[0])
         {
@@ -177,9 +159,97 @@ public:
         return Response(ResponseType.Status, "ok");
     }
 
+public:
+
+    this(string filename, size_t capacity = 10000)
+    {
+        import std.stdio : File;
+        import std.algorithm : filter;
+
+        auto state = appender!(V[]);
+        state.reserve(capacity);
+        auto reader = File(filename, "r");
+        auto lines = reader.byLine().filter!"!a.empty"();
+        foreach (ref line; lines)
+        {
+            state ~= line.to!string;
+        }
+
+        _state = state[];
+    }
+
+    /**
+     * Receive requests from any threads and execute that
+     */
+    void runRequestReceiver()
+    {
+        for (;;)
+        {
+            auto result = appender!(Response[]);
+            result.reserve(10);
+
+            auto requests = receiveOnly!(Tid, string);
+            // debug writeln(__FILE__ ~ ":" ~ __LINE__ ~ requests);
+            if (requests[1] == "close")
+                break;
+            foreach (request; requests[1].split("&&"))
+            {
+                result ~= execute(request);
+            }
+
+            requests[0].send(result[].idup);
+        }
+    }
+
     override string toString()
     {
         return to!string(_state);
     }
 
+}
+
+/**
+ * Interface for transactions in DBs
+ */
+interface Transaction
+{
+
+    /**
+     * Save transaction to log
+     */
+    Transaction opCall(string request);
+
+    /**
+     * Run all requests from log
+     */
+    Response[] commit();
+}
+
+class TransactionImpl : Transaction
+{
+    private auto _log = appender!(string[]);
+    private Tid _dbId;
+
+    this(Tid dbId)
+    {
+        _dbId = dbId;
+    }
+
+    override Transaction opCall(string request)
+    {
+        _log ~= request;
+        return this;
+    }
+
+    override Response[] commit()
+    {
+        _dbId.send(thisTid, _log[].joiner(" && ").to!string);
+
+        return receiveOnly!(immutable(Response)[]).dup;
+    }
+}
+
+Transaction newTransaction(Tid dbId)
+{
+    return new TransactionImpl(dbId);
 }
